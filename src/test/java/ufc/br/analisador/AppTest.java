82cad9a5b293a31cccc6a78cd8f463fb415a5dcf
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-03-17 08:33:11 -0300

    Primeiro commit

---
+++ src/test/java/ufc/br/analisador/AppTest.java
@@ -0,0 +1,20 @@
+package ufc.br.analisador;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
